f3418aac1209f4b2dc8df2ef6dc45b32c936ea8b
usayuki <masapp.01a@gmail.com>
2020-05-18 09:31:45 +0900

    プロジェクト作成

---
+++ WebRTCHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebRTCHandsOn
+//
+//  Created by 石川 雅之 on 2020/05/18.
+//  Copyright © 2020 usayuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
